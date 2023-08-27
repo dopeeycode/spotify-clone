130ae8276b757b948ce19c59bbba8418bdbb68d8
Italo <italodopeey@gmail.com>
2023-08-27 01:29:32 -0300

    feat: button upload hidden while modal is open

--- components/AddSongMobile.tsx
+++ components/AddSongMobile.tsx
@@ -4,11 +4,13 @@ import useAuthModal from "@/hooks/useAuthModal"
 import useUploadModal from "@/hooks/useUploadModal"
 import { useUser } from "@/hooks/useUser"
 import { BiPlus } from "react-icons/bi"
+import { twMerge } from "tailwind-merge"
 
 const AddSongMobile = () => {
   const uploadModal = useUploadModal()
   const authModal = useAuthModal()
   const { user } = useUser()
+  const { isOpen } = useUploadModal()
 
   function handleUpload(){
     if (!user) {
@@ -19,7 +21,9 @@ const AddSongMobile = () => {
   }
 
   return (
-    <div className="flex md:hidden bg-green-500 rounded-full p-2 absolute z-10 right-5 bottom-5">
+    <div className={twMerge("flex md:hidden bg-green-500 rounded-full p-2 absolute z-10 right-5 bottom-5", 
+    isOpen && "hidden")}
+      >
       <button onClick={handleUpload}>
         <BiPlus size={30} />
       </button>
