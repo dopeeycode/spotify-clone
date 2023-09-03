be11c35a765ffb60ce635e90e41c4449a8c14a90
Italo <italodopeey@gmail.com>
2023-09-03 19:57:55 -0300

    fix: search component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,27 +1,32 @@
-import getSongsByTitle from "@/actions/getSongsByTitle"
-import Header from "@/components/Header"
-import SearchInput from "@/components/SearchInput"
-import SearchContent from "./components/SearchContent"
+import getSongsByTitle from "@/actions/getSongsByTitle";
+import SearchInput from "@/components/SearchInput";
+import Header from "@/components/Header";
+
+import SearchContent from "./components/SearchContent";
+
+export const revalidate = 0;
 
 interface SearchProps {
-  searchParams:{
-    title: string
-  }
-}
+  searchParams: { title: string }
+};
 
-
-const Search = async ({ searchParams }: SearchProps )=> {
-  const songs = await getSongsByTitle(searchParams.title)
+const Search = async ({ searchParams }: SearchProps) => {
+  const songs = await getSongsByTitle(searchParams.title);
 
   return (
-    <div
-      className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-hidden"
+    <div 
+      className="
+        bg-neutral-900 
+        rounded-lg 
+        h-full 
+        w-full 
+        overflow-hidden 
+        overflow-y-auto
+      "
     >
-      <Header className="from-bg-neutral-900">
-        <div
-          className="mb-2 flex flex-col gap-y-6"
-        >
-          <h1 className="text-white text-3xl mt-2 font-semibold">
+      <Header className="from-neutral-600">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">
             Search
           </h1>
           <SearchInput />
@@ -29,7 +34,7 @@ const Search = async ({ searchParams }: SearchProps )=> {
       </Header>
       <SearchContent songs={songs} />
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
